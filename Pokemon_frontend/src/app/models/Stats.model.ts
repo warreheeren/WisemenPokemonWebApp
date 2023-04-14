e45b503d6142cd4b3bd2381c8d06b5cb6f6f435c
warreheeren <warre.heeren@hotmail.com>
2023-04-14 14:23:25 +0200

    final styling added

--- Pokemon_frontend/src/app/models/Stats.model.ts
+++ Pokemon_frontend/src/app/models/Stats.model.ts
@@ -4,13 +4,4 @@ export class Stats {
   base_stat: number;
   effort: number;
   stat: Stat;
-  get color(): string {
-    if (this.base_stat > 50) {
-      console.log('groen');
-      return '#FF0000';
-    } else {
-      console.log('rood');
-      return '#00FF00';
-    }
-  }
 }
